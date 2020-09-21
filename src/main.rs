5be4cb05b365dcfd762997d29754891924698f95
Prince <princechauhan1992@hotmail.com>
2020-09-21 08:43:30 +0530

    crate example

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,7 @@
-use std::io;
+extern crate rand;
+use rand::Rng;
 fn main() {
-    println!("Guess the number!");
-    println!("Please input your guess.");
-    let mut guess = String::new();
-    io::stdin().read_line(&mut guess)
-        .expect("Failed to read line");
-    println!("You guessed: {}", guess);
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+    println!("You guessed: {}", secret_number);
 }
     
