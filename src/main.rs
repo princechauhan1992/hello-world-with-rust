89cf4826a526d8240ff895de601850426fb3c90f
Prince <princechauhan1992@hotmail.com>
2020-09-21 21:52:47 +0530

    added secret number game

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,30 @@
 extern crate rand;
 use rand::Rng;
+use std::io;
+use std::cmp::Ordering;
 fn main() {
     let secret_number = rand::thread_rng().gen_range(1, 101);
-    println!("You guessed: {}", secret_number);
+    loop {
+        let mut guess = String::new();
+        println!("Please Input your guess");
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
+    
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => { 
+                println!("Please enter a valid Number!");
+                continue;
+            }
+        };
+        println!("You guessed: {}", guess);
+        match guess.cmp(&secret_number) { 
+            Ordering::Less => println!("Too small!"), 
+            Ordering::Greater => println!("Too big!"), 
+            Ordering::Equal => { println!("You win!");
+                break; 
+            },
+        }
+    }
 }
     
